4ce05cbaed0278de102a8545a09297020f0741ab
João Dias <joaoeduardodias123@gmail.com>
2026-03-23 11:18:38 -0300

    feat:  alter api url

--- electron/constants.ts
+++ electron/constants.ts
@@ -9,6 +9,6 @@ export const VITE_DEV = process.env['VITE_DEV_SERVER_URL']
 export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
 export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
 
-export const SYSTEM_API_BASE_URL = 'http://localhost:3333'
-export const WS_URL = 'ws://localhost:3333/ws/agent'
+export const SYSTEM_API_BASE_URL = 'https://api-cold-monitor.onrender.com'
+export const WS_URL = 'wss://api-cold-monitor.onrender.com/ws/agent'
 export const DEVICE_AUTH_ENDPOINT = '/devices/auth/login'
